5257df2746d32c2e70093857ec25b26bc253dddb
Yasunori Shimura <yasu0710@gmail.com>
2022-04-02 23:28:34 +0900

    Add support for IRv11 to ir_featuremap_extractor

--- ir_featuremap_extractor.py
+++ ir_featuremap_extractor.py
@@ -5,9 +5,15 @@ import copy
 import pickle
 
 import cv2
-import xml.etree.ElementTree as et
 
-from openvino.inference_engine import IECore
+from openvino.preprocess import PrePostProcessor
+from openvino.runtime import AsyncInferQueue, Core, InferRequest, Layout, Type
+
+type_table = { Type.f16: 'f16', Type.f32: 'f32', Type.f64: 'f64', Type.bf16: 'bf16',
+               Type.i16: 'i16', Type.i32: 'i32', Type.i64: 'i64', Type.i8  : 'i8',
+               Type.u16: 'u16', Type.u32: 'u32', Type.u64: 'u64', Type.u8  : 'u8',
+               Type.bitwidth: 'bit', Type.boolean: 'bool' }
+
 
 def splitFileName(file):
     dirname, filename = os.path.split(file)
@@ -26,60 +32,18 @@ def readBIN(model):
     return weight
 
 
-def findNodeFromXML(xmltree, nodeid):
-    root = xmltree.getroot()
-    layers = root.find('layers')
-    for layer in layers.findall('layer'):
-        if int(layer.attrib['id']) == nodeid:
-            return layer
-    return None
-
-
 def modifyXMLForFeatureVectorProbing(xmltree, nodeid):
-    xmlcopy = copy.deepcopy(xmltree)
-    layer = findNodeFromXML(xmlcopy, nodeid)
-
-    # obtain output port information of the target node (port # and dims)
-    outport      = layer.find('output').find('port')
-    outport_id   = int(outport.attrib['id'])
-    outport_prec = outport.attrib['precision']
-    outport_dims = outport.findall('dim')
-    outport_dims_string = ""
-    for dim in outport_dims:
-        outport_dims_string += et.tostring(dim).decode('utf-8')
-
-    # generate XML strings
-    dummyLayer = """
-    		<layer id="9999" name="featuremap_checker_dummy_node" type="Result" version="opset1">
-			<input>
-				<port id="0">
-                {}
-				</port>
-			</input>
-		</layer>
-    """.format(outport_dims_string)
-    dummyEdge = '		<edge from-layer="{}" from-port="{}" to-layer="9999" to-port="0"/>'.format(nodeid, outport_id)
-
-    # modify XML to make a dummy branch path for feature map extraction
-    xmlcopy.find('layers').append(et.fromstring(dummyLayer))
-    xmlcopy.find('edges').append(et.fromstring(dummyEdge))
-
-    # return the modified XML and the name of the target node (specified by 'nodeid')
-    return xmlcopy, layer.attrib['name']
-
+    pass
 
 
 # TODO: You need to modify this function to make this fit to your model
 #       E.g. - If your model uses multiple inputs, you need to prepare input data for those inputs
 #            - If your model requires non-image data, you need to implement appropiate data preparation code and preprocessing for it
-def prepareInputs(net_inputs, args):
+def prepareInputs(ir_model, args):
     input_data = {}
 
-    input_blob_names  = list(net_inputs.keys())
-
-    input0Name = input_blob_names[0]
-    input0Info = net_inputs[input0Name]
-    N,C,H,W = input0Info.tensor_desc.dims
+    input0Name = ir_model.input().get_any_name()
+    N,C,H,W = ir_model.input().shape
     img = cv2.imread(args.input)    # default = image.jpg
     img = cv2.resize(img, (W, H))
     img = img.transpose((2, 0, 1))
@@ -92,46 +56,41 @@ def prepareInputs(net_inputs, args):
 
 
 def main(args):
-    originalXML = readXML(args.model)
-    weight = readBIN(args.model)
 
     print('node# : nodeName')
     feature_vectors = {}
-    ie = IECore()
-    root = originalXML.getroot()
-    layers = root.find('layers')
-    for layer in layers.findall('layer'):
-        nodeid = int(layer.attrib['id'])
-        nodetype = layer.attrib['type']
-        if nodetype in ['Const']: # , 'ShapeOf', 'Convert', 'StridedSlice', 'PriorBox']:
+    ie = Core()
+
+    ir_model = ie.read_model(args.model)
+    ops = ir_model.get_ops()
+    del ir_model
+
+    for op_idx, op in enumerate(ops):
+        op_name  = op.get_friendly_name()
+        op_type  = op.type_info.name
+        if op_type in [ 'Constant', 'Parameter', 'Result' ]:
             continue
-        if not layer.find('output') is None:
-            nodeName = layer.attrib['name']
-            outputport = layer.find('output').find('port')
-            proc = outputport.attrib['precision']
-            dims = []
-            for dim in outputport.findall('dim'):                       # extract shape information
-                dims.append(dim.text)
 
-            modifiedXML, targetNodeName = modifyXMLForFeatureVectorProbing(originalXML, nodeid)
-            XMLstr = et.tostring(modifiedXML.getroot())
-            print('{} : {}'.format(nodeid, targetNodeName))
+        op_outputs = op.outputs()
+        for out_idx, out in enumerate(op_outputs):
+            out_shape = out.get_shape()
+            out_shape = list(out_shape) if len(out_shape)>0 else []
+            out_elem_type = type_table[out.get_element_type()]
 
-            net = ie.read_network(XMLstr, weight, init_from_buffer=True)
-            try:
-                exenet = ie.load_network(net, args.device)
-            except RuntimeError:
-                #et.dump(modifiedXML)
-                print('*** RuntimeError: load_network() -- Skip node \'{}\' - \'{}\''.format(targetNodeName, nodetype))
-                continue
+            print(op_idx, op_name, op_type, out_idx, out_shape, out_elem_type)
 
-            inputs = prepareInputs(net.input_info, args)    # ToDo: Prepare inupts for inference. User may need to modify this function to generate appropriate input for the specific model.
-            res = exenet.infer(inputs)[nodeName]
+            ir_model = ie.read_model(args.model)
+            ir_model.add_outputs([(op_name, out_idx)])     # Add probe to snoop the feature map
+            compiled_model = ie.compile_model(ir_model, args.device)
 
-            feature_vectors[nodeName] = [proc, dims, res]
-            #print(nodeName, res)
-            del exenet
-            del net
+            inputs = prepareInputs(ir_model, args)         # ToDo: Prepare inupts for inference. User may need to modify this function to generate appropriate input for the specific model.
+            res = compiled_model.infer_new_request(inputs)
+            keys = list(res.keys())
+            if len(keys)>1:
+                #print(res[keys[1]])
+                feature_vectors[op_name] = [out_idx, out_elem_type, out_shape, list(res[keys[1]])]
+            del compiled_model
+            del ir_model
 
     dirname, filename = os.path.split(args.model)
     basename, extname = os.path.splitext(filename)
@@ -144,7 +103,7 @@ def main(args):
 if __name__ == "__main__":
     print('*** OpenVINO feature map extractor')
     print('@@@ This program takes \'image.jpg\' and supply to the 1st input blob as default.')
-    print('@@@ In case your model requires special data input, you need to modify \'prepareInputs()\' function to meet the requirements.')
+    print('@@@ In case your model requires special input data, you need to modify \'prepareInputs()\' function to meet the requirements.')
 
     parser = argparse.ArgumentParser()
     parser.add_argument('-m', '--model', type=str, help='input IR model path')
